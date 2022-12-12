a6717342d030758004fa27322393d0f6ce0bf37b
zack <zz.leghchim97@gmail.com>
2022-12-13 00:24:31 +0100

    first commit

---
+++ src/main/java/or/sid/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,29 @@
+package or.sid.customerservice;
+
+import or.sid.customerservice.entities.Customer;
+import or.sid.customerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+    public static void main(String[] args) { SpringApplication.run(CustomerServiceApplication.class, args); }
+
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository, RepositoryRestConfiguration restConfiguration){
+        restConfiguration.exposeIdsFor(Customer.class);
+        return args -> {
+            customerRepository.save(new Customer(null,"Mouad","mouad@gmail.com"));
+            customerRepository.save(new Customer(null,"Yassine","yassine@gmail.com"));
+            customerRepository.save(new Customer(null,"Aymane","aymane@gmail.com"));
+            customerRepository.findAll().forEach(c->{
+                System.out.println(c.toString());
+            });
+
+        };
+
+    }
+}
